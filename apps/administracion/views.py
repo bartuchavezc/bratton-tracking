7a862efa9fb170a4130688dfa372f7239db6fe14
utrab <bartolomechavezcampano2@gmail.com>
2019-02-11 20:01:51 -0300

    v14

--- apps/administracion/views.py
+++ apps/administracion/views.py
@@ -41,7 +41,7 @@ class ChangeUserToActivated(LoginRequiredMixin, PermissionRequiredMixin ,UpdateV
     permission_required = 'users.validate_user'
     form_class = ChangeUserToActivatedForm
     template_name = 'administracion/users/user_update_page.html'
-    success_url = reverse_lazy('administracion:change_solicitud')
+    success_url = reverse_lazy('administracion:solicitudes')
 
 
 class CargasView(LoginRequiredMixin, PermissionRequiredMixin ,TemplateView):
