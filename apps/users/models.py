664a25e2692315983372f81b0159c7533de47e64
utrab <bartolomechavezcampano2@gmail.com>
2019-02-12 12:01:51 -0300

    commit v21

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,7 +5,7 @@ from apps.administracion.models import Customer, Vessel
 # Create your models here.
 
 class CustomUser(AbstractUser):
-        customer = models.OneToOneField(Customer, on_delete=False, null=True)
+        customer = models.OneToOneField(Customer, on_delete=models.CASCADE, null=True)
         is_bratton_admin = models.BooleanField(default=False)
         is_validated = models.BooleanField(default=False)
         def __str__(self):
